6e6d23c99e589abe1ceb5dad09bd7496b76f5c43
pancake-zinc <pancake@quoll.finance>
2024-02-06 15:44:59 +0800

    [euclid] add gen-withdrawal-fields-proof

---
+++ euclid-tools/scripts/gen-withdrawal-fields-proof.mjs
@@ -0,0 +1,27 @@
+#!/usr/bin/env zx
+
+const {
+    validatorIndex,
+    chainID
+} = argv;
+
+if (!validatorIndex) {
+    console.error('Please specify validatorIndex, e.g. 707739 for Goerli node');
+    process.exit(1);
+}
+
+if (!chainID) {
+    console.error('Please specify chainID, e.g. 5 for Goerli');
+    process.exit(1);
+}
+
+console.log('Building generation executable...');
+await $`cd ../generation && go build`;
+
+console.log('Generating validator fields proof...');
+await $`cd .. && ./generation/generation -command WithdrawalFieldsProof \
+  -oracleBlockHeaderFile "./euclid-tools/data/block_header.json" \
+  -stateFile "./euclid-tools/data/slot.json" \
+  -validatorIndex ${validatorIndex} \
+  -outputFile "./euclid-tools/data/withdrawal-fields-proof.json" \
+  -chainID ${chainID}`;
